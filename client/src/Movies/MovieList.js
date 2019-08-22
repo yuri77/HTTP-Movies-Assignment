1b8590c35c357eafb51809bcc223c1cb16e57a0e
yurika sakae <yurika.sun@gmail.com>
2019-08-22 13:55:18 -0700

    initial commit

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,32 +1,57 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard";
-export default class MovieList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
-    };
-  }
 
-  componentDidMount() {
+export const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
     axios
       .get("http://localhost:5000/api/movies")
-      .then(res => this.setState({ movies: res.data }))
-      .catch(err => console.log(err.response));
-  }
+      .then(res => {
+        console.log("movielist res", res);
+        setMovies(res.data);
+      })
+      .catch(err => console.log("movieList err", err));
+  }, []);
 
-  render() {
-    return (
-      <div className="movie-list">
-        {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="movie-list">
+      {movies.map(movie => (
+        <MovieDetails key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
+
+// export default class MovieList extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       movies: []
+//     };
+//   }
+
+//   componentDidMount() {
+//     axios
+//       .get("http://localhost:5000/api/movies")
+//       .then(res => this.setState({ movies: res.data }))
+//       .catch(err => console.log(err.response));
+//   }
+
+//   render() {
+//     return (
+//       <div className="movie-list">
+//         {this.state.movies.map(movie => (
+//           <MovieDetails key={movie.id} movie={movie} />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
 
 function MovieDetails({ movie }) {
   return (
