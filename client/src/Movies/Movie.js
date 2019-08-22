fb0c763701cc08e5d0a293dc770aabbd32b13c1a
yurika sakae <yurika.sun@gmail.com>
2019-08-22 14:29:26 -0700

    refactored movie.js WIP movie edit form

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,48 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard";
-export default class Movie extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movie: null
-    };
-  }
 
-  componentDidMount() {
-    this.fetchMovie(this.props.match.params.id);
-  }
+const Movie = props => {
+  console.log("Movies props", props);
+  const [movie, setMovie] = useState([]);
+  const id = props.match.params.id;
 
-  componentWillReceiveProps(newProps) {
-    if (this.props.match.params.id !== newProps.match.params.id) {
-      this.fetchMovie(newProps.match.params.id);
-    }
-  }
-
-  fetchMovie = id => {
+  const fetchMovie = id => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
-      .then(res => this.setState({ movie: res.data }))
+      .then(res => {
+        console.log("Movie res", res);
+        setMovie(res.data);
+      })
       .catch(err => console.log(err.response));
   };
 
-  saveMovie = () => {
-    const addToSavedList = this.props.addToSavedList;
-    addToSavedList(this.state.movie);
+  useEffect(() => {
+    fetchMovie(id);
+  }, [id]);
+
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie);
+    props.history.push("/");
   };
 
-  render() {
-    if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
-    }
+  return (
+    <>
+      {!movie && <div>Loading movie information...</div>}
 
-    return (
       <div className="save-wrapper">
-        <MovieCard movie={this.state.movie} />
-        <div className="save-button" onClick={this.saveMovie}>
+        <MovieCard movie={movie} />
+        <div className="save-button" onClick={saveMovie}>
           Save
         </div>
       </div>
-    );
-  }
-}
+    </>
+  );
+};
+
+export default Movie;
+
+// export default class Movie extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       movie: null
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.fetchMovie(this.props.match.params.id);
+//   }
+
+//   componentWillReceiveProps(newProps) {
+//     if (this.props.match.params.id !== newProps.match.params.id) {
+//       this.fetchMovie(newProps.match.params.id);
+//     }
+//   }
+
+//   fetchMovie = id => {
+//     axios
+//       .get(`http://localhost:5000/api/movies/${id}`)
+//       .then(res => this.setState({ movie: res.data }))
+//       .catch(err => console.log(err.response));
+//   };
+
+//   saveMovie = () => {
+//     const addToSavedList = this.props.addToSavedList;
+//     addToSavedList(this.state.movie);
+//   };
+
+//   render() {
+//     if (!this.state.movie) {
+//       return <div>Loading movie information...</div>;
+//     }
+
+//     return (
+//       <div className="save-wrapper">
+//         <MovieCard movie={this.state.movie} />
+//         <div className="save-button" onClick={this.saveMovie}>
+//           Save
+//         </div>
+//       </div>
+//     );
+//   }
+// }
